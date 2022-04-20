e4ed28e2f7871b7444937d832241f976e4f0e5b9
Luiz Henrique <7henrique18@gmail.com>
2022-04-20 00:20:39 -0300

    :gear: Remove unused

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,13 +7,6 @@ import { ChakraProvider, CSSReset } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
 import { theme } from '../styles/theme'
-import { Header } from '../components/Header'
-import { Footer } from '../components/Footer'
-
-import { AuthContextProvider } from '../context/AuthContext'
-import { ProductsContextProvider } from '../context/ProductsContext'
-import { UploadContextProvider } from '../context/UploadContext'
-import { CartContextProvider } from '../context/CartContext'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   React.useEffect(() => {
@@ -26,23 +19,14 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <ChakraProvider theme={theme}>
-      <AuthContextProvider>
-        <ProductsContextProvider>
-          <UploadContextProvider>
-            <CartContextProvider>
-              <CSSReset />
-              <NextProgress
-                delay={300}
-                options={{ showSpinner: true }}
-                color="#04BF8A"
-              />
-              <Header />
-              <Component {...pageProps} />
-              <Footer />
-            </CartContextProvider>
-          </UploadContextProvider>
-        </ProductsContextProvider>
-      </AuthContextProvider>
+      <CSSReset />
+      <NextProgress
+        delay={300}
+        options={{ showSpinner: true }}
+        color="#04BF8A"
+      />
+
+      <Component {...pageProps} />
     </ChakraProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,5 @@
-
-import { api } from '../services/api'
-import { Product } from '../types/product'
-
-type HomeProps = {
-  products: Product[]
-} & NextPage
-
-const Home = (props: HomeProps) => {
-  const { products } = props
-
+const Home = () => {
   return <h1>oi</h1>
 }
 
 export default Home
-
