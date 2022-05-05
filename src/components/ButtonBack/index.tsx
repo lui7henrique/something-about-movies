0ea8c2d54392dc4ee073cadebd6cda1e32f31155
Luiz Henrique <7henrique18@gmail.com>
2022-05-04 22:27:28 -0300

    :sparkles: Change button back

--- src/components/ButtonBack/index.tsx
+++ src/components/ButtonBack/index.tsx
@@ -1,12 +1,14 @@
 // Vendors
 
 // Components
-import { Box, IconButton, IconButtonProps } from '@chakra-ui/react'
+import { ButtonProps } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import { IoMdArrowBack } from 'react-icons/io'
+import { Locale } from 'types/locale'
+import { Button } from 'components/Button'
 
 // Types
-type ButtonBackProps = Omit<IconButtonProps, 'aria-label'>
+type ButtonBackProps = ButtonProps
 
 /*
 |-----------------------------------------------------------------------------
@@ -24,8 +26,14 @@ export const ButtonBack = (props: ButtonBackProps) => {
   |
   |
   */
-  const { back } = useRouter()
+  const { back, locale } = useRouter()
 
+  const label: {
+    [key in Locale]: string
+  } = {
+    'en-US': 'Back',
+    'pt-BR': 'Voltar'
+  }
   /*
   |-----------------------------------------------------------------------------
   | States
@@ -66,13 +74,14 @@ export const ButtonBack = (props: ButtonBackProps) => {
   |
   */
   return (
-    <IconButton
+    <Button
+      label={label[locale as Locale]}
       aria-label="back"
-      icon={<IoMdArrowBack size={20} />}
+      leftIcon={<IoMdArrowBack size={20} />}
       onClick={back}
       zIndex={5}
       borderRadius="sm"
       {...props}
-    />
+    ></Button>
   )
 }
