4ea97b2c1edebdf7c997284473d93a3933f09615
Luiz Henrique <7henrique18@gmail.com>
2022-05-22 01:38:36 -0300

    :sparkles: Fix template columsn media list

--- src/layout/Private/components/MediaList/index.tsx
+++ src/layout/Private/components/MediaList/index.tsx
@@ -78,13 +78,13 @@ export const MediaList = (props: MediaListProps) => {
 
   const templateColumns = poster
     ? {
-        base: 'repeat(2, 1fr)',
+        base: 'repeat(1, 1fr)',
         lg: 'repeat(4, 1fr)',
         xl: 'repeat(6, 1fr)',
         '2xl': 'repeat(8, 1fr)'
       }
     : {
-        base: 'repeat(2, 1fr)',
+        base: 'repeat(1, 1fr)',
         lg: 'repeat(3, 1fr)',
         xl: 'repeat(4, 1fr)',
         '2xl': 'repeat(5, 1fr)'
